629d42e00db7c6ec1a9e1693ba49fed07794584f
Gorm <gdsoucie@gmail.com>
2020-06-14 18:41:08 -0400

    post now posts a new empty workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,7 @@
 var path = require("path");   //do I need this?
 // const { db } = require("../models/workout");
 const workouts = require("../models/workout.js");
+const { Workout } = require("../models/index.js");
 
 
 module.exports = function (app) {
@@ -24,9 +25,8 @@ module.exports = function (app) {
   //this worked in mongodb, created an exersize...
 
 
-  app.get("/api/workouts", (req, res) => {
+  app.get("/api/workouts", (req, res) => {  // works, may need refinement...
     workouts.find()
-     
       .then(dbRes => {
         console.log(dbRes);
         res.json(dbRes);
@@ -38,15 +38,57 @@ module.exports = function (app) {
   );
   // workout.js will grab the last object from this... so it should be an array...
 
-  app.post("/api/workouts", (req, res) => { //FIX ME
-    console.log(req.body);
+  app.post("/api/workouts", (req, res) => { // probably works!, may need fixin
+    // console.log(req.body);
     //Workout.create(...data...)
-    workouts.create
+    const work = new Workout(req.body);
+    //check if body is good data...
+    workouts.create(work)
+      .then(workres => {
+        res.json(workres);
+      })
+      .catch(err => {
+        res.json(err);
+      });
 
-   
+    /*
+
+app.post("/submit", ({ body }, res) => {
+  const user = new User(body);
+  user.coolifier();
+  user.makeCool();
+
+  User.create(user)
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+
+     db.Book.create(body)
+    .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+    .then(dbLibrary => {
+      res.json(dbLibrary);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+    
+    */
+
+
+
   }
   );
   app.put("/api/workouts", (req, res) => {  // FIX ME
+    //get id from database
+    //
+    workouts.find()
+    
+
 
     // do database stuff here
     let myData = "not implemented";
@@ -59,6 +101,7 @@ module.exports = function (app) {
   app.get("/api/workouts/range", (req, res) => {
     res.json("test");
     //workout.functions...
+    // get...
   }
   );
   // next step; investigate models, pull up example code.
@@ -102,8 +145,9 @@ module.exports = function (app) {
   });
   */
 
-
-
-
+  //will need a delete?
+  // ^^^
+//app.delete("/api/workouts/:id", req, res)
+//workouts.findByIdAndDelete(body.id)
 
 };
